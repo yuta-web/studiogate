b2e719baccd008da67ad6c2a5b68f96610bd890f
ゆーや <baseball300@icloud.com>
2022-05-02 21:44:46 +0900

    no message

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,47 +1,93 @@
 'use strict';
 // スムーズスクロール
 {
-  $(function(){
-    $('.slider1').slick({
-      autoplay: true, // 自動でスクロール
-      autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
-      speed: 5000, // スライドが流れる速度を設定
-      cssEase: "linear", // スライドの流れ方を等速に設定
-      slidesToShow: 2, // 表示するスライドの数
-      swipe: false, // 操作による切り替えはさせない
-      arrows:  true, // 矢印非表示
-      pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
-      pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
-      responsive: [
-        {
-          breakpoint: 750,
-          settings: {
-            slidesToShow: 2, // 画面幅750px以下でスライド3枚表示
+  if ($(window).width() > 768) {
+    $(function(){
+      $('.slider1').slick({
+        autoplay: true, // 自動でスクロール
+        autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+        speed: 5000, // スライドが流れる速度を設定
+        cssEase: "linear", // スライドの流れ方を等速に設定
+        slidesToShow: 2, // 表示するスライドの数
+        swipe: false, // 操作による切り替えはさせない
+        arrows:  true, // 矢印非表示
+        pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+        vertical: true,
+        pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+        responsive: [
+          {
+            breakpoint: 750,
+            settings: {
+              slidesToShow: 2, // 画面幅750px以下でスライド3枚表示
+            }
           }
-        }
-      ]
-    });
-    $('.slider2').slick({
-      autoplay: true, // 自動でスクロール
-      autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
-      speed: 5000, // スライドが流れる速度を設定
-      cssEase: "linear", // スライドの流れ方を等速に設定
-      slidesToShow: 2, // 表示するスライドの数
-      swipe: false, // 操作による切り替えはさせない
-      arrows:  true, // 矢印非表示
-      rtl: true,
-      pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
-      pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
-      responsive: [
-        {
-          breakpoint: 750,
-          settings: {
-            slidesToShow: 2, // 画面幅750px以下でスライド3枚表示
+        ]
+      });
+      $('.slider2').slick({
+        autoplay: true, // 自動でスクロール
+        autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+        speed: 5000, // スライドが流れる速度を設定
+        cssEase: "linear", // スライドの流れ方を等速に設定
+        slidesToShow: 2, // 表示するスライドの数
+        swipe: false, // 操作による切り替えはさせない
+        arrows:  true, // 矢印非表示
+        rtl: true,
+        vertical: true,
+        pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+        pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+        responsive: [
+          {
+            breakpoint: 750,
+            settings: {
+              slidesToShow: 2, // 画面幅750px以下でスライド3枚表示
+            }
           }
-        }
-      ]
+        ]
+      });
     });
-  });
+  } else {
+    $(function(){
+      $('.slider1').slick({
+        autoplay: true, // 自動でスクロール
+        autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+        speed: 5000, // スライドが流れる速度を設定
+        cssEase: "linear", // スライドの流れ方を等速に設定
+        slidesToShow: 2, // 表示するスライドの数
+        swipe: false, // 操作による切り替えはさせない
+        arrows:  true, // 矢印非表示
+        pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+        pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+        responsive: [
+          {
+            breakpoint: 750,
+            settings: {
+              slidesToShow: 2, // 画面幅750px以下でスライド3枚表示
+            }
+          }
+        ]
+      });
+      $('.slider2').slick({
+        autoplay: true, // 自動でスクロール
+        autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+        speed: 5000, // スライドが流れる速度を設定
+        cssEase: "linear", // スライドの流れ方を等速に設定
+        slidesToShow: 2, // 表示するスライドの数
+        swipe: false, // 操作による切り替えはさせない
+        arrows:  true, // 矢印非表示
+        rtl: true,
+        pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+        pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+        responsive: [
+          {
+            breakpoint: 750,
+            settings: {
+              slidesToShow: 2, // 画面幅750px以下でスライド3枚表示
+            }
+          }
+        ]
+      });
+    });
+  }
 }
 
 { 
