5afb86e3cc170e7be30ed27f0460298c47c40a3a
ゆーや <baseball300@icloud.com>
2022-05-02 10:37:38 +0900

    no message

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,55 +1,56 @@
 'use strict';
 
-// {
-//   $(function(){
-//     $('.slider1').slick({
-//       autoplay: true, // 自動でスクロール
-//       autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
-//       speed: 5000, // スライドが流れる速度を設定
-//       cssEase: "linear", // スライドの流れ方を等速に設定
-//       slidesToShow: 2, // 表示するスライドの数
-//       swipe: false, // 操作による切り替えはさせない
-//       arrows: false, // 矢印非表示
-//       vertical: true, // 流れる方向を決める
-//       // rtl: true,
-//       // pauseOnHover: false,
-//       pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
-//       pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
-//       responsive: [
-//         {
-//           breakpoint: 750,
-//           settings: {
-//             slidesToShow: 3, // 画面幅750px以下でスライド3枚表示
-//           }
-//         }
-//       ]
-//     });
-//   });
+{
 
-//   $(function(){
-//     $('.slider2').slick({
-//       autoplay: true, // 自動でスクロール
-//       autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
-//       speed: 5000, // スライドが流れる速度を設定
-//       cssEase: "linear", // スライドの流れ方を等速に設定
-//       slidesToShow: 2, // 表示するスライドの数
-//       swipe: false, // 操作による切り替えはさせない
-//       arrows: false, // 矢印非表示
-//       vertical: true, // 流れる方向を決める
-//       // pauseOnHover: false,
-//       pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
-//       pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
-//       responsive: [
-//         {
-//           breakpoint: 750,
-//           settings: {
-//             slidesToShow: 3, // 画面幅750px以下でスライド3枚表示
-//           }
-//         }
-//       ]
-//     });
-//   });
-// }
+  $(function(){
+    $('.slider').slick({
+      autoplay: true, // 自動でスクロール
+      autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+      speed: 5000, // スライドが流れる速度を設定
+      cssEase: "linear", // スライドの流れ方を等速に設定
+      slidesToShow: 2, // 表示するスライドの数
+      swipe: false, // 操作による切り替えはさせない
+      arrows: false, // 矢印非表示
+      // vertical: true, // 流れる方向を決める
+      // rtl: true,
+      // pauseOnHover: false,
+      pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+      pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+      responsive: [
+        {
+          breakpoint: 750,
+          settings: {
+            slidesToShow: 3, // 画面幅750px以下でスライド3枚表示
+          }
+        }
+      ]
+    });
+  });
+
+  // $(function(){
+  //   $('.slider2').slick({
+  //     autoplay: true, // 自動でスクロール
+  //     autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+  //     speed: 5000, // スライドが流れる速度を設定
+  //     cssEase: "linear", // スライドの流れ方を等速に設定
+  //     slidesToShow: 2, // 表示するスライドの数
+  //     swipe: false, // 操作による切り替えはさせない
+  //     arrows: false, // 矢印非表示
+  //     // vertical: true, // 流れる方向を決める
+  //     // pauseOnHover: false,
+  //     pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+  //     pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+  //     responsive: [
+  //       {
+  //         breakpoint: 750,
+  //         settings: {
+  //           slidesToShow: 3, // 画面幅750px以下でスライド3枚表示
+  //         }
+  //       }
+  //     ]
+  //   });
+  // });
+}
 
 // スムーズスクロール
 { 
